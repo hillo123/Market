36967b25ac9411ecd96ae4079026d5b5ea04436b
Eduardo Medina <eduardo.medina@ext.leroymerlin.es>
2020-08-13 23:48:29 +0200

    cleaning

--- app/src/main/java/com/dabyz/market/MainActivity.kt
+++ app/src/main/java/com/dabyz/market/MainActivity.kt
@@ -8,39 +8,26 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_sign_up.*
 
 class MainActivity : AppCompatActivity() {
-
     private val mail: String = "eamedina@gmail.com"
-    val fragments = hashMapOf(
-        R.id.miCart to CartFragment(),
-        R.id.miProducts to ProductsFragment(mail),
-        R.id.miStores to StoresFragment()
+    private val fragments = hashMapOf(
+        R.id.miCart to CartFragment(), R.id.miProducts to ProductsFragment(mail), R.id.miStores to StoresFragment()
     )
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportFragmentManager.beginTransaction().apply {
-            val mail =
-                getSharedPreferences("dabyz.market", Context.MODE_PRIVATE).getString("mail", null)
+            val mail = getSharedPreferences("dabyz.market", Context.MODE_PRIVATE).getString("mail", null)
             if (mail == null)
                 replace(R.id.flFragment, SignUpFragment())
             else
                 replace(R.id.flFragment, ProductsFragment(mail))
             commit()
         }
-
         bottomNavigationView.setOnNavigationItemSelectedListener {
-            when (it.itemId) {
-                R.id.miCart -> nav(CartFragment())
-                R.id.miProducts -> nav(ProductsFragment(mail))
-                R.id.miStores -> nav(StoresFragment())
-            }
+            supportFragmentManager.beginTransaction()
+                .apply { replace(R.id.flFragment, fragments[it.itemId] as Fragment); commit() }
             true
         }
     }
-
-    private fun nav(fragment: Fragment) = supportFragmentManager.beginTransaction()
-        .apply { replace(R.id.flFragment, fragment); commit() }
-
 }
