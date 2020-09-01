9af9fbe800251f0c0217a7450bfcd68ef18d1cb2
Eduardo Medina <eduardo.medina@ext.leroymerlin.es>
2020-09-01 21:16:08 +0200

    add data classes for busines an d customer relation

--- app/src/main/java/com/dabyz/market/models/StoreModel.kt
+++ app/src/main/java/com/dabyz/market/models/StoreModel.kt
@@ -5,12 +5,18 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ListenerRegistration
+import java.util.*
+import kotlin.collections.ArrayList
 
 data class Product(var title: String = "", var title2: String = "", var price: Long = 0, var photo: String = "", var fileName: String = "")
 data class Business(
     var name: String = "", val mail: String = "", var password: String = "", var phone: String = "", var address: String = "",
     var refs: ArrayList<Product> = ArrayList()
 )
+
+data class Line (val product:Product, var quantity:Int)
+data class Order (val date: Date = Date(), var orderLines: ArrayList<Line> = ArrayList())
+data class C2B(val business: String, val customer: String, var orders: ArrayList<Order> = ArrayList())
 
 class StoreModel : ViewModel() {
     private var businessListener: ListenerRegistration? = null
@@ -22,7 +28,7 @@ class StoreModel : ViewModel() {
         }
 
     val selectedBusiness = MutableLiveData<Business>()
-    fun initCurrentBusiness() {
+    private fun initCurrentBusiness() {
         businessListener?.remove()
         businessListener = dbBusiness.document(actualStore).addSnapshotListener { snapshot, e ->
             e?.let { Log.w("Model", "Listen failed.", e); return@addSnapshotListener }
