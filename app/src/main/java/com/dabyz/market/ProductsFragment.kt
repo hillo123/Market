f6b65c9d73d191c866296e582cbcba98e17c3380
Eduardo Medina <eduardo.medina@ext.leroymerlin.es>
2020-09-04 00:04:33 +0200

    add quantities to product list

--- app/src/main/java/com/dabyz/market/ProductsFragment.kt
+++ app/src/main/java/com/dabyz/market/ProductsFragment.kt
@@ -10,6 +10,7 @@ import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.dabyz.market.models.Line
 import com.dabyz.market.models.Product
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.card_product.view.*
@@ -26,41 +27,46 @@ class ProductsFragment() : Fragment(R.layout.fragment_products) {
         }
         main.storeModel.selectedBusiness.observe(main as LifecycleOwner, Observer { business ->
             main.txFragmentTitle.text = business.name
-            productsAdapter.apply { products = business.refs; notifyDataSetChanged() }
+            productsAdapter.apply {
+                // TODO enrich business.refs with cart
+                productQttys = business.refs.map { Line(it, 0) }
+                notifyDataSetChanged()
+            }
         })
     }
 
     inner class ProductsListAdapter() : RecyclerView.Adapter<ProductsListAdapter.ItemHolder>() {
-        var products = listOf<Product>()
+        var productQttys = listOf<Line>()
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductsListAdapter.ItemHolder =
             ItemHolder(LayoutInflater.from(main).inflate(R.layout.card_product, parent, false))
 
-        override fun getItemCount(): Int = products.size
+        override fun getItemCount(): Int = productQttys.size
 
         override fun onBindViewHolder(holder: ProductsListAdapter.ItemHolder, position: Int) =
-            holder.setData(products[position])
+            holder.setData(productQttys[position])
 
         inner class ItemHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-            lateinit var product: Product
+            lateinit var productQtty: Line
 
             init {
                 itemView.btnRemove2Cart.setOnClickListener {
-                    main.storeModel.add2Cart(product, -1)
+                    main.storeModel.add2Cart(productQtty.product!!, -1)
                 }
                 itemView.btnAdd2Cart.setOnClickListener {
-                    main.storeModel.add2Cart(product, 1)
+                    main.storeModel.add2Cart(productQtty.product!!, 1)
                 }
             }
 
-            fun setData(product: Product) {
-                this.product = product
-                product.apply {
+            fun setData(productQtty: Line) {
+                this.productQtty = productQtty
+                productQtty.product?.apply {
                     itemView.etTitle.text = title
                     itemView.etTitle2.text = title2
                     itemView.etPrice.text = price.toString()
                     Glide.with(main).load(photo).into(itemView.imgProduct)
                 }
+                itemView.tvQtty.text = productQtty.quantity.toString()
             }
         }
     }
